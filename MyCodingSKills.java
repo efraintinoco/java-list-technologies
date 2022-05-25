d23bdbc962a453ffffef4dc4aa74fd34e6d9b10a
efraintinoco <oakcliff_21@yahoo.com>
2022-05-25 17:34:27 -0500

    add solution

---
+++ MyCodingSKills.java
@@ -0,0 +1,10 @@
+class MyCodingSkills {
+    public static void main(String [] args) {
+        
+        String[] skills = { "CLI", "GIT", "HTML", "CSS", "JavaScript", "TypeScript", "Angular", "Java" };
+
+        for (String skill : skills) {
+            System.out.println("Kamp Kyle has made me a master of " + skill +" !");
+        }
+    }
+}
